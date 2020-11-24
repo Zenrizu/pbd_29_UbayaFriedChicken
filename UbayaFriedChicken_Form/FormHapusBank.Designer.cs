f08b222a1e80c9992a841484d170967b10fb19fd
GWOUBAYA <72499735+GWOUBAYA@users.noreply.github.com>
2020-11-24 21:40:29 +0800

    Create FormHapusBank.Designer.cs

---
+++ UbayaFriedChicken_Form/FormHapusBank.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace form
+{
+    partial class FormHapusBank
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHapusBank));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelNama = new System.Windows.Forms.Label();
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.labelId = new System.Windows.Forms.Label();
+            this.buttonHapus = new System.Windows.Forms.Button();
+            this.labelWarna = new System.Windows.Forms.Label();
+            this.buttonKeluar = new System.Windows.Forms.Button();
+            this.buttonKosongi = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.labelNama);
+            this.panel1.Controls.Add(this.textBoxNama);
+            this.panel1.Controls.Add(this.textBoxId);
+            this.panel1.Controls.Add(this.labelId);
+            this.panel1.Location = new System.Drawing.Point(12, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(444, 122);
+            this.panel1.TabIndex = 44;
+            // 
+            // labelNama
+            // 
+            this.labelNama.AutoSize = true;
+            this.labelNama.BackColor = System.Drawing.Color.Transparent;
+            this.labelNama.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNama.Location = new System.Drawing.Point(34, 64);
+            this.labelNama.Name = "labelNama";
+            this.labelNama.Size = new System.Drawing.Size(87, 16);
+            this.labelNama.TabIndex = 26;
+            this.labelNama.Text = "Nama Bank :";
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(127, 63);
+            this.textBoxNama.Multiline = true;
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(289, 30);
+            this.textBoxNama.TabIndex = 25;
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(127, 29);
+            this.textBoxId.Multiline = true;
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(66, 28);
+            this.textBoxId.TabIndex = 23;
+            this.textBoxId.TextChanged += new System.EventHandler(this.textBoxId_TextChanged);
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.BackColor = System.Drawing.Color.Transparent;
+            this.labelId.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelId.ForeColor = System.Drawing.Color.Black;
+            this.labelId.Location = new System.Drawing.Point(55, 30);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(66, 16);
+            this.labelId.TabIndex = 22;
+            this.labelId.Text = "ID Bank :";
+            // 
+            // buttonHapus
+            // 
+            this.buttonHapus.BackColor = System.Drawing.Color.White;
+            this.buttonHapus.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonHapus.ForeColor = System.Drawing.Color.Red;
+            this.buttonHapus.Location = new System.Drawing.Point(12, 195);
+            this.buttonHapus.Name = "buttonHapus";
+            this.buttonHapus.Size = new System.Drawing.Size(117, 41);
+            this.buttonHapus.TabIndex = 45;
+            this.buttonHapus.Text = "HAPUS";
+            this.buttonHapus.UseVisualStyleBackColor = false;
+            this.buttonHapus.Click += new System.EventHandler(this.buttonHapus_Click);
+            // 
+            // labelWarna
+            // 
+            this.labelWarna.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelWarna.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWarna.ForeColor = System.Drawing.Color.White;
+            this.labelWarna.Location = new System.Drawing.Point(12, 9);
+            this.labelWarna.Name = "labelWarna";
+            this.labelWarna.Size = new System.Drawing.Size(444, 41);
+            this.labelWarna.TabIndex = 48;
+            this.labelWarna.Text = "HAPUS BANK";
+            this.labelWarna.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonKeluar
+            // 
+            this.buttonKeluar.BackColor = System.Drawing.Color.White;
+            this.buttonKeluar.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKeluar.ForeColor = System.Drawing.Color.Red;
+            this.buttonKeluar.Location = new System.Drawing.Point(345, 195);
+            this.buttonKeluar.Name = "buttonKeluar";
+            this.buttonKeluar.Size = new System.Drawing.Size(111, 41);
+            this.buttonKeluar.TabIndex = 47;
+            this.buttonKeluar.Text = "KELUAR";
+            this.buttonKeluar.UseVisualStyleBackColor = false;
+            this.buttonKeluar.Click += new System.EventHandler(this.buttonKeluar_Click);
+            // 
+            // buttonKosongi
+            // 
+            this.buttonKosongi.BackColor = System.Drawing.Color.White;
+            this.buttonKosongi.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKosongi.ForeColor = System.Drawing.Color.Red;
+            this.buttonKosongi.Location = new System.Drawing.Point(174, 195);
+            this.buttonKosongi.Name = "buttonKosongi";
+            this.buttonKosongi.Size = new System.Drawing.Size(128, 41);
+            this.buttonKosongi.TabIndex = 46;
+            this.buttonKosongi.Text = "KOSONGI";
+            this.buttonKosongi.UseVisualStyleBackColor = false;
+            this.buttonKosongi.Click += new System.EventHandler(this.buttonKosongi_Click);
+            // 
+            // FormHapusBank
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(470, 245);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonHapus);
+            this.Controls.Add(this.labelWarna);
+            this.Controls.Add(this.buttonKeluar);
+            this.Controls.Add(this.buttonKosongi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormHapusBank";
+            this.Text = "Hapus Bank";
+            this.Load += new System.EventHandler(this.FormHapusBank_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelNama;
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Button buttonHapus;
+        private System.Windows.Forms.Label labelWarna;
+        private System.Windows.Forms.Button buttonKeluar;
+        private System.Windows.Forms.Button buttonKosongi;
+    }
+}
