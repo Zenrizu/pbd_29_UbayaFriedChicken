d71ee9e9e89fe090480dc867c98cbcaa32da7041
GWOUBAYA <72499735+GWOUBAYA@users.noreply.github.com>
2020-11-24 21:40:18 +0800

    Create FormDaftarTopUp.Designer.cs

---
+++ UbayaFriedChicken_Form/FormDaftarTopUp.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace form
+{
+    partial class FormDaftarTopUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDaftarTopUp));
+            this.panelWarna = new System.Windows.Forms.Panel();
+            this.textBoxCari = new System.Windows.Forms.TextBox();
+            this.comboBoxCari = new System.Windows.Forms.ComboBox();
+            this.labelKeterangan = new System.Windows.Forms.Label();
+            this.dataGridViewData = new System.Windows.Forms.DataGridView();
+            this.labelWarna = new System.Windows.Forms.Label();
+            this.buttonUbah = new System.Windows.Forms.Button();
+            this.buttonHapus = new System.Windows.Forms.Button();
+            this.buttonKeluar = new System.Windows.Forms.Button();
+            this.buttonTambah = new System.Windows.Forms.Button();
+            this.panelWarna.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelWarna
+            // 
+            this.panelWarna.BackColor = System.Drawing.Color.Transparent;
+            this.panelWarna.Controls.Add(this.textBoxCari);
+            this.panelWarna.Controls.Add(this.comboBoxCari);
+            this.panelWarna.Controls.Add(this.labelKeterangan);
+            this.panelWarna.Location = new System.Drawing.Point(12, 56);
+            this.panelWarna.Name = "panelWarna";
+            this.panelWarna.Size = new System.Drawing.Size(487, 36);
+            this.panelWarna.TabIndex = 37;
+            // 
+            // textBoxCari
+            // 
+            this.textBoxCari.Location = new System.Drawing.Point(277, 10);
+            this.textBoxCari.Name = "textBoxCari";
+            this.textBoxCari.Size = new System.Drawing.Size(191, 20);
+            this.textBoxCari.TabIndex = 2;
+            // 
+            // comboBoxCari
+            // 
+            this.comboBoxCari.BackColor = System.Drawing.Color.White;
+            this.comboBoxCari.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCari.FormattingEnabled = true;
+            this.comboBoxCari.Items.AddRange(new object[] {
+            "ID Transaksi",
+            "Jumlah",
+            "ID Pelanggan",
+            "ID Bank"});
+            this.comboBoxCari.Location = new System.Drawing.Point(132, 10);
+            this.comboBoxCari.Name = "comboBoxCari";
+            this.comboBoxCari.Size = new System.Drawing.Size(139, 21);
+            this.comboBoxCari.TabIndex = 1;
+            // 
+            // labelKeterangan
+            // 
+            this.labelKeterangan.AutoSize = true;
+            this.labelKeterangan.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKeterangan.Location = new System.Drawing.Point(14, 13);
+            this.labelKeterangan.Name = "labelKeterangan";
+            this.labelKeterangan.Size = new System.Drawing.Size(112, 13);
+            this.labelKeterangan.TabIndex = 0;
+            this.labelKeterangan.Text = "Cari Berdasarkan :";
+            // 
+            // dataGridViewData
+            // 
+            this.dataGridViewData.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewData.Location = new System.Drawing.Point(12, 108);
+            this.dataGridViewData.Name = "dataGridViewData";
+            this.dataGridViewData.Size = new System.Drawing.Size(487, 231);
+            this.dataGridViewData.TabIndex = 38;
+            // 
+            // labelWarna
+            // 
+            this.labelWarna.BackColor = System.Drawing.Color.Red;
+            this.labelWarna.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWarna.ForeColor = System.Drawing.Color.White;
+            this.labelWarna.Location = new System.Drawing.Point(12, 9);
+            this.labelWarna.Name = "labelWarna";
+            this.labelWarna.Size = new System.Drawing.Size(487, 35);
+            this.labelWarna.TabIndex = 36;
+            this.labelWarna.Text = "DAFTAR TOP UP";
+            this.labelWarna.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonUbah
+            // 
+            this.buttonUbah.BackColor = System.Drawing.Color.White;
+            this.buttonUbah.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUbah.ForeColor = System.Drawing.Color.Red;
+            this.buttonUbah.Location = new System.Drawing.Point(166, 345);
+            this.buttonUbah.Name = "buttonUbah";
+            this.buttonUbah.Size = new System.Drawing.Size(94, 26);
+            this.buttonUbah.TabIndex = 42;
+            this.buttonUbah.Text = "UBAH";
+            this.buttonUbah.UseVisualStyleBackColor = false;
+            // 
+            // buttonHapus
+            // 
+            this.buttonHapus.BackColor = System.Drawing.Color.White;
+            this.buttonHapus.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonHapus.ForeColor = System.Drawing.Color.Red;
+            this.buttonHapus.Location = new System.Drawing.Point(266, 345);
+            this.buttonHapus.Name = "buttonHapus";
+            this.buttonHapus.Size = new System.Drawing.Size(94, 26);
+            this.buttonHapus.TabIndex = 41;
+            this.buttonHapus.Text = "HAPUS";
+            this.buttonHapus.UseVisualStyleBackColor = false;
+            // 
+            // buttonKeluar
+            // 
+            this.buttonKeluar.BackColor = System.Drawing.Color.White;
+            this.buttonKeluar.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKeluar.ForeColor = System.Drawing.Color.Red;
+            this.buttonKeluar.Location = new System.Drawing.Point(405, 345);
+            this.buttonKeluar.Name = "buttonKeluar";
+            this.buttonKeluar.Size = new System.Drawing.Size(94, 26);
+            this.buttonKeluar.TabIndex = 40;
+            this.buttonKeluar.Text = "KELUAR";
+            this.buttonKeluar.UseVisualStyleBackColor = false;
+            this.buttonKeluar.Click += new System.EventHandler(this.buttonKeluar_Click);
+            // 
+            // buttonTambah
+            // 
+            this.buttonTambah.BackColor = System.Drawing.Color.White;
+            this.buttonTambah.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTambah.ForeColor = System.Drawing.Color.Red;
+            this.buttonTambah.Location = new System.Drawing.Point(66, 345);
+            this.buttonTambah.Name = "buttonTambah";
+            this.buttonTambah.Size = new System.Drawing.Size(94, 26);
+            this.buttonTambah.TabIndex = 39;
+            this.buttonTambah.Text = "TAMBAH";
+            this.buttonTambah.UseVisualStyleBackColor = false;
+            this.buttonTambah.Click += new System.EventHandler(this.buttonTambah_Click);
+            // 
+            // FormDaftarTopUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(512, 384);
+            this.Controls.Add(this.panelWarna);
+            this.Controls.Add(this.dataGridViewData);
+            this.Controls.Add(this.labelWarna);
+            this.Controls.Add(this.buttonUbah);
+            this.Controls.Add(this.buttonHapus);
+            this.Controls.Add(this.buttonKeluar);
+            this.Controls.Add(this.buttonTambah);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormDaftarTopUp";
+            this.Text = "Daftar Transaksi";
+            this.panelWarna.ResumeLayout(false);
+            this.panelWarna.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelWarna;
+        private System.Windows.Forms.TextBox textBoxCari;
+        private System.Windows.Forms.ComboBox comboBoxCari;
+        private System.Windows.Forms.Label labelKeterangan;
+        private System.Windows.Forms.DataGridView dataGridViewData;
+        private System.Windows.Forms.Label labelWarna;
+        private System.Windows.Forms.Button buttonUbah;
+        private System.Windows.Forms.Button buttonHapus;
+        private System.Windows.Forms.Button buttonKeluar;
+        private System.Windows.Forms.Button buttonTambah;
+    }
+}
